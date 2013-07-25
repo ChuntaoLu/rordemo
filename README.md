e88b9959e6b9f221c370b39bb75ba952dbe69a68
chuntaolu <chuntaolu13@gmail.com>
2013-07-25 17:53:17 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Ruby on Rails Tutorial study
+This is my RoR study project following *Roby on Rails Tutorial* by Michael
+Hart.
+Much appreciation for his free ebook.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '37988d9015269f5aad42075b434621278ef73c74de6bb27956d753b752d45c7b201dc7dc7903bf94fcddfd4815c19665c501c1de60ad03ea488e0387c774a65e'

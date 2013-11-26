5a21922caf19127f32ef8317da0de8ef3b1b7668
ChuntaoLu <chuntaolu13@gmail.com>
2013-11-26 01:05:01 -0500

    Finish Chapter 7

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  #  Returns the Gravator for the given user
+  def gravatar_for(user, options={ size: 50 })
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "http://www.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name "Lu"
+    email "lu@aol.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
